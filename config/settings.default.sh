bd53f634c64485eeed03296d93bdf503c3453294
qthenoz <tech_director@tiz.fr>
2020-04-02 12:07:17 +0200

    feat(trash file) : we add other potential trash files

--- config/settings.default.sh
+++ config/settings.default.sh
@@ -5,12 +5,16 @@
 #
 
 PATH_TO_CLEAN="/var/www/"
-TRASHES[0]="*.tar.gz"
-TRASHES[1]="*.zip"
-TRASHES[2]="*.sql"
-TRASHES[3]="*.sql.*"
-TRASHES[4]="adminer.php"
-TRASHES[5]="dploy.yaml"
+TRASHES[0]="*.tar."
+TRASHES[1]="*.tar.*"
+TRASHES[2]="*.zip"
+TRASHES[3]="*.sql"
+TRASHES[4]="*.sql.*"
+TRASHES[5]=".phar"
+TRASHES[6]="adminer.php"
+TRASHES[7]="dploy.yaml"
+TRASHES[8]="srdb.cli.php"
+TRASHES[9]="bigdump.php"
 
 MAIL_SUBJECT="Trash cleaner"
 MAIL_NOTIFICATION="tech@tiz.fr"
